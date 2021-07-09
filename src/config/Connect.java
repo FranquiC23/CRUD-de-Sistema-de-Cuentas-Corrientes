a282c5bf2ad385be87c43b468c16ebd4c782ac78
Frank Quispe <fquispecah@unsa.edu.pe>
2021-07-08 22:18:10 -0500

    Proyecto Actualizado

--- src/config/Connect.java
+++ src/config/Connect.java
@@ -10,10 +10,10 @@ public class Connect {
 			// Connection to Data Base
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/BD_CUENTAS_CORRIENTES", "root", "toor");
-			//System.out.println("Connected successfully..................");
+			System.out.println("Connected successfully..................");
 			
 		} catch (Exception e) {
-			System.out.println("Error Can't connected");
+			e.printStackTrace();
 		}
 	}
 
